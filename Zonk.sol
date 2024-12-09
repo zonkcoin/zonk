6d3291d3538c3ba63adf24d0ecf35fb5e45dacb8
zonkcoin <jonas.weber.1000@gmail.com>
2024-12-09 12:48:07 -0700

    Update Zonk.sol
    added .sol code.

--- Zonk.sol
+++ Zonk.sol
@@ -1,1 +1,22 @@
-Will be posted in here in near future.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Zonk is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 1_000_000_000 * 10**4;
+
+    constructor() ERC20("Zonk", "ZONK") Ownable(msg.sender) {
+        _mint(msg.sender, 500_000_000 * 10**4);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 4;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds max supply");
+        _mint(to, amount);
+    }
+}
